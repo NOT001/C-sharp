2649e209fa74d037e42589f1214a6ae57d45b5a3
NOT001 <nnguyenha2002@gmail.com>
2023-10-10 23:40:17 +0700

    initial commit

---
+++ Lap4/Database_Connection/Database_Connection/ConnectSQLDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database_Connection
+{
+    public partial class ConnectSQLDatabase : Form
+    {
+        string connectionString = "server=localhost;user id=nguyenha;password=Ha1122002;database=student_management";
+        SqlConnection conn = new SqlConnection();
+
+        public ConnectSQLDatabase()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (conn != null && conn.State==ConnectionState.Open) 
+            {
+                conn.Close();
+                MessageBox.Show("Successful Disconnetion!!!");
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                MessageBox.Show("Successful connect");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fail to connect to server \n " + ex.Message);
+            }
+        }
+    }
+}
